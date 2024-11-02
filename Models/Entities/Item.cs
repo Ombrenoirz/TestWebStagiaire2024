877e239d69037818eaf9df0f5756850c7e33689e
Keven Ouellet <kevenouellet@hotmail.com>
2024-11-02 12:30:10 -0400

    Initialized Database

---
+++ Models/Entities/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestWebStagiaire2024.Models.Entities
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestWebStagiaire2024.Models.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public List<Item> GroceryList { get; set; } = new List<Item>();
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
